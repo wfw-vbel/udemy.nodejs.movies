4908312b3f08c8fb53a689c5b37da8e537c3fb7b
Slava Belliaev <Viacheslav.Beliaev@workflowwise.com>
2019-11-27 12:03:38 +0200

    add dynamic data

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const moviesData = require('./add-movie')
+
 const router = express.Router();
 const views = "./views";
 
 router.get('/', (req, res, next) => {
-    res.render('home', {pageTitle: "Home page", path: "home"});
+    res.render('home', {
+        pageTitle: "Home page",
+        movies: moviesData.movies,
+        path: "home"});
 })
 
 module.exports = router;
