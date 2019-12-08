585797991ef7c2a88bdff32d14ec4eb7545a04b6
VBeliaev <vbeliaev@luxoft.com>
2019-12-09 00:48:37 +0200

    delete unnecessary files

--- models/backup/movie.js
+++
@@ -1,9 +0,0 @@
-const Item = require('./item');
-
-module.exports = class Movie extends Item{
-    constructor(t, p = "", s = {}, c={}) {
-        super(t, p = "", s = {}, c={});
-        this.type = "movies"
-    };
-};
-
--- models/backup/show.js
+++
@@ -1,8 +0,0 @@
-const Item = require('./item');
-
-module.exports = class Show extends Item{
-    constructor(t, p = "", s = {}, c={}) {
-        super(t, p = "", s = {}, c={});
-        this.type = "shows"
-    };
-};
