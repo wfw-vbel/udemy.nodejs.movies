377b94cbcb43eb8e6721d35e9d9ee81c049c16d9
VBeliaev <vbeliaev@luxoft.com>
2019-12-16 22:31:20 +0200

    add ClearDb connection settings

--- data/database.js
+++ data/database.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    'datamoviz',
-    'root',
-    'EXua8ups1',
+    'heroku_c730721ba3a5524',
+    'bc69a3e8bdc150',
+    '16a63097',
     {
         dialect: 'mysql',
-        host: 'localhost'
+        host: 'eu-cdbr-west-02.cleardb.net'
         ,logging: false
     });
 
