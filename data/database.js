aac412b475c38ff0459aa9d5895bb0283b8ca7a4
wfw-vbel <48201553+wfw-vbel@users.noreply.github.com>
2019-12-17 11:15:22 +0200

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    'heroku_c730721ba3a5524',
-    'bc69a3e8bdc150',
-    '16a63097',
+    '5IU45193DS',
+    '5IU45193DS',
+    'ZbDgPUdcA0',
     {
         dialect: 'mysql',
-        host: 'eu-cdbr-west-02.cleardb.net'
+        host: 'remotemysql.com'
         ,logging: false
     });
 
